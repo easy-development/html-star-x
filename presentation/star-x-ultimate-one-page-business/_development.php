39344cc3d843a149d38f234a66feeff2a4b81f23
Andrei-Robert Rusu <rusuandreirobert@gmail.com>
2014-12-11 02:02:33 +0200

    Initial Github Commit

---
+++ presentation/star-x-ultimate-one-page-business/_development.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Star-X Ultimate One Page Business Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <script type="text/javascript" src="assets/scripts/jquery-1.10.2.js"></script>
+    <script src="assets/scripts/bootstrap.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="assets/scripts/jquery-knob.js"></script>
+    <script type="text/javascript" src="assets/scripts/retina.js"></script>
+
+    <link type="text/css" href="assets/style/bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="assets/style/font-awesome.css" rel="stylesheet">
+    <link type="text/css" href="assets/style/hint.css" rel="stylesheet">
+    <link type="text/css" href="assets/style/animate.css" rel="stylesheet">
+    <link type="text/css" href="assets/style/story-zen.css" rel="stylesheet">
+
+	<link rel="stylesheet/less" type="text/css" href="assets/style-development/style.less"/>
+	<script src="assets/scripts/less-1.7.0.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript" src="assets/scripts/story-box/story-box.min.js"></script>
+    <script type="text/javascript" src="assets/scripts/story-tale/story-tale.min.js"></script>
+    <script type="text/javascript" src="assets/scripts/template-helper.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="assets/scripts/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+  <body data-spy="scroll" data-target="#scrollSpyMenu" data-offset="150">
+    <?php require("parts/_body-loading-animation.html");?>
+
+    <?php require("parts/_primary-line-separator.html");?>
+
+    <?php require("parts/navigation.html"); ?>
+
+    <?php require("parts/_secondary-line-separator.html");?>
+
+    <?php require("parts/slider.html"); ?>
+
+    <?php require("parts/call-to-action.html"); ?>
+
+    <?php require("parts/headline.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/features-2-row.html"); ?>
+
+    <?php require("parts/separator-subscription-video.html");?>
+
+    <?php require("parts/business-services-2.html") ?>
+
+    <?php require("parts/separator-subscription.html"); ?>
+
+    <?php require("parts/features-devices.html"); ?>
+
+    <?php require("parts/separator-testimonial.html"); ?>
+
+    <?php require("parts/features-macbook-pro.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/features-iphone.html"); ?>
+
+    <?php require("parts/separator-achievements.html"); ?>
+
+    <?php require("parts/device-show-case.html");?>
+
+    <?php require("parts/separator-quotation.html");?>
+
+    <?php require("parts/text.html");?>
+
+    <?php require("parts/separator-headline.html");?>
+
+    <?php require("parts/business-services.html"); ?>
+
+    <?php require("parts/separator-progress.html");?>
+
+    <?php require("parts/price-table.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/price-table-2.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/features-row.html"); ?>
+
+    <?php require("parts/features-footer.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/skills-bar.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/features-mac.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/our-clients-row.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/github-repositories.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/the-team-row.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/the-team-compact.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/the-team-row-2.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/the-team-row-3.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/the-team-compact-2.html");?>
+
+    <?php require("parts/separator-features.html"); ?>
+
+    <?php require("parts/_typography-heading.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/_typography-list-items.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/_typography-columns.html"); ?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <h2 id="new-components-separator" class="text-primary text-center">New Components</h2>
+
+    <?php require("parts/the-team-row-creative.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/price-table-creative.html");?>
+
+    <?php require("parts/_elegant-line-separator.html"); ?>
+
+    <?php require("parts/subscription.html");?>
+
+    <?php require("parts/footer.html");?>
+
+    <?php require("parts/footer-bar.html");?>
+  </body>
+</html>
